fb36378dbacd2f6a266c5c56d26de1d7ec17d63e
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-27 10:24:49 +0530

    Add project files.

---
+++ FileIOOperationsDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOOperationsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.FileExists();
+            Program.ReadAllLine();
+            Program.ReadAllText();
+            //Calling Method
+            //Stream Reader Writer Operations
+            StreamReaderWriterOps streamReaderWriterOps = new StreamReaderWriterOps();
+            streamReaderWriterOps.ReadFromStreamReader();
+            streamReaderWriterOps.WriteUsingStreamWriter();
+            
+        }
+
+        public static void FileExists()
+        {
+            string path = @"C:\Users\ASUS\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.ReadLine();
+
+        }
+        public static void ReadAllLine()
+        {
+            string path = @"C:\Users\ASUS\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            string[] lines;
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+            Console.ReadKey();
+        }
+        public static void ReadAllText()
+        {
+
+            string path = @"C:\Users\ASUS\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+            Console.ReadKey();
+        }
+    }
+
+}
+
+
---
+++ FileIOOperationsDemo/StreamReaderWriterOps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileIOOperationsDemo
+{
+    public class StreamReaderWriterOps
+    {
+        public void WriteUsingStreamWriter()
+        {
+            string path = @"C:\Users\ASUS\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hello World - .Net is Awesome");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+        }
+        public void ReadFromStreamReader()
+        {
+            string path = @"C:\Users\ASUS\source\repos\FileIOOperationsDemo\FileIOOperationsDemo\Example.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = " ";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
